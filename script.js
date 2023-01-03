77a3c9e406424ea8ae4a8c82f6a1f986352855dd
Danilo Guirra <daniloguirra@hotmail.com>
2023-01-02 22:57:27 -0300

    Hello World 2023 for luck

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Bem Vindo 2023");
+console.log("Que esse seja repleto de desafios na minha carreira profissional");
+console.log("Que eu consiga atingir todos meus objetivos nesse ramo que admiro tanto");
+console.log("Consoles.logs registrado com sucesso!!");
